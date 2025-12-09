126c24ef9edf333a9ca14c781597963c6525f0b4
juviZ138 <quachlydientam@gmail.com>
2025-12-10 03:59:03 +0700

    SearchService + MongoDB,  synochorus http communicate between 2 services

---
+++ src/SearchService/Services/AuctionServiceHttpClient.cs
@@ -0,0 +1,22 @@
+using System;
+using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService.Services;
+
+public class AuctionServiceHttpClient(HttpClient httpClient, IConfiguration config)
+{
+    public async Task<List<Item>> GetItemForSearchDb()
+    {
+        var lastUpdated = await DB.Find<Item, string>()
+            .Sort(x => x.Descending(a => a.UpdatedAt))
+            .Project(x => x.UpdatedAt.ToString())
+            .ExecuteFirstAsync();
+
+
+        return await httpClient.GetFromJsonAsync<List<Item>>
+            (config["AuctionServiceUrl"] + "/api/auctions?date=" + lastUpdated);
+
+
+    }
+}
