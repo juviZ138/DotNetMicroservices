b9796dd7bc0038ccbf34c1b603487f910abf0a4d
juviZ138 <quachlydientam@gmail.com>
2025-12-15 11:40:27 +0700

    Gateway Service + YARP

---
+++ src/AuctionService/Consumers/AuctionFinishedConsumer.cs
@@ -0,0 +1,28 @@
+using System;
+using AuctionService.Data;
+using Contracts;
+using MassTransit;
+
+namespace AuctionService.Consumers;
+
+public class AuctionFinishedConsumer(AuctionDbContext dbContext) : IConsumer<AuctionFinished>
+{
+    public async Task Consume(ConsumeContext<AuctionFinished> context)
+    {
+        Console.WriteLine("--> Consuming Auction Finished");
+
+        var auction = await dbContext.Auctions.FindAsync(context.Message.AuctionId);
+
+        if (context.Message.ItemSold)
+        {
+            auction.Winner = context.Message.Winner;
+            auction.SoldAmount = context.Message.Amount;
+        }
+
+        auction.Status = auction.SoldAmount > auction.ReservePrice
+            ? Entities.Status.Finished
+            : Entities.Status.ReserveNotMet;
+
+        await dbContext.SaveChangesAsync();
+    }
+}
