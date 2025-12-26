427ea1ed90c4079274ca58c98e224e58a742a532
juviZ138 <quachlydientam@gmail.com>
2025-12-26 15:56:45 +0700

    new: Notification Service (SignalR)

---
+++ src/NotificationService/Consumers/BidPlacedConsumer.cs
@@ -0,0 +1,16 @@
+using System;
+using MassTransit;
+using Microsoft.AspNetCore.SignalR;
+using NotificationService.Hubs;
+
+namespace NotificationService.Consumers;
+
+public class BidPlaced(IHubContext<NotificationHub> hubContext) : IConsumer<BidPlaced>
+{
+    public async Task Consume(ConsumeContext<BidPlaced> context)
+    {
+        Console.WriteLine("--> Bid placed message received");
+
+        await hubContext.Clients.All.SendAsync("BidPlaced", context.Message);
+    }
+}
